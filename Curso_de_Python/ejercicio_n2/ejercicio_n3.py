7720ae9c23f5494055eed80f85eb9b059be6300d
Alfonso Gonzalez <alpolo1991@gmail.com>
2022-05-23 12:32:36 -0500

    Actulizando .gitignore y se desarrolla ejercicio N1 o N3 del video session 2.1

---
+++ Curso_de_Python/ejercicio_n2/ejercicio_n3.py
@@ -0,0 +1,12 @@
+'''
+# Ejercicio N3
+
+En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, 
+a su vez, tendrás que imprimir también el tipo de la variable por consola.
+'''
+
+txt_variable = "Hola Mundo"
+txt_name = "Hola Estoy aprendiendo Python en Open Bootcamp"
+
+print(txt_variable + "\n" + txt_name)
+print(type(txt_variable))
