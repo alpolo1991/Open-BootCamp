086c4eeb89d44007e0ffe8de00351c15b79ae8cd
Alfonso Gonzalez <alpolo1991@gmail.com>
2022-05-23 12:38:12 -0500

    Desarrollando Ejercicio N 4 Video Session 2.1

---
+++ Curso_de_Python/ejercicio_n2/ejercicio_n4.py
@@ -0,0 +1,15 @@
+'''
+# Ejercicio N4
+
+Modifica la variable del anterior ejercicio en la consola de python y 
+después muestrala por consola para ver la modificación de la variable.
+'''
+
+int_variable = 200
+float_variable = 30.3
+txt_name = "Hola Estoy aprendiendo Python en Open Bootcamp"
+
+print(int_variable)
+print(txt_name)
+print(type(int_variable))
+print(type(float_variable))
