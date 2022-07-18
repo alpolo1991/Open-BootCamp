ba02e78b31f00db1efd1516cdac536f690b3583c
Alfonso Gonzalez <63266487+alpolo1991@users.noreply.github.com>
2022-07-18 10:58:42 -0500

    Modificacion Archivos Tema 12 - DJango

--- Curso_de_Python/tema_12/AppCine/cineUser/views.py
+++ Curso_de_Python/tema_12/AppCine/cineUser/views.py
@@ -3,11 +3,11 @@ from .models import Director, Pelicula
 
 # Create your views here.
 def home(request):
-    directors = Director.objects.all()
+    #directors = Director.objects.all()
     peliculas = Pelicula.objects.all()
     
     context = {
-        'directors': directors,
+        #'directors': directors,
         'peliculas': peliculas,
     }
     return render(request, 'cineUser/index.html', context)
