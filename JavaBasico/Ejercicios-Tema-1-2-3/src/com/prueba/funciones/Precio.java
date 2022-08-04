d44328c8147bd53239cc41d99de0487ecce27fcf
Alfonso Gonzalez Posso <63266487+alpolo1991@users.noreply.github.com>
2022-08-04 11:02:20 -0500

    Resolviendo Ejercicios Tema N 2

--- JavaBasico/Ejercicios-Tema-1-2-3/src/com/prueba/funciones/Precio.java
+++ JavaBasico/Ejercicios-Tema-1-2-3/src/com/prueba/funciones/Precio.java
@@ -1,2 +1,19 @@
-package com.prueba.funciones;public class Precio {
+package com.prueba.funciones;
+/**
+ * Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+ */
+public class Precio {
+    public static void main(String[] args) {
+
+        float iva = 19f;
+        float precio = 1200f;
+
+        float resultado = getPrecio(precio, iva);
+
+        System.out.println("El precio con el IVA del "+iva+"% incluido es de: "+resultado);
+    }
+
+    public static float getPrecio(float precio, float iva){
+        return precio * iva;
+    }
 }
--- JavaBasico/Ejercicios-Tema-1-2-3/src/com/prueba/operadores/Operadores.java
+++ JavaBasico/Ejercicios-Tema-1-2-3/src/com/prueba/operadores/Operadores.java
@@ -1,4 +1,35 @@
 package com.prueba.operadores;
 
 public class Operadores {
+    public static void main(String[] args) {
+
+        // aritmeticos:
+        /*
+        +
+        -
+        *
+        /
+        % -> Modulo
+         */
+
+        /*
+        comparación:
+        > mayor que
+        < menor que
+        >= mayor igual que
+        <= menor igual que
+        == igual que
+        === IMPORTANTE: EN JAVA NO EXISTE!!!!
+         */
+
+        /*
+        Lógicos
+        and &&
+        or ||
+         */
+
+        // operadores
+        // incremento ++
+        // decremento --
+    }
 }
